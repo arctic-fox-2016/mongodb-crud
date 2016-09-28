73a90e2b72fc6da6121c6870ccdd06a0d3fb187f
Andrew <atandiawan@gmail.com>
2016-09-28 19:48:46 +0700

    belum selesai

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,16 @@ let Model = require('../models/model.js')
 router.use(bodyParser.urlencoded({extended:true}))
 router.set('view-engine','ejs')
 
-router.get('/transactions', function(req,res,next){
+router.get('/', function(req,res,next){
   mongoose.model('transactions').find(function(err, result){
-    res.send(result)
-  })
-})
-
-router.get('/books', function(req,res,next){
-  mongoose.model('books').find(function(err, result){
-    res.render('result.ejs', {books: result})
-  })
-})
-
-router.get('/customers', function(req,res,next){
-  mongoose.model('customers').find(function(err, result){
-    res.send(result)
+    let transactions = result
+    mongoose.model('books').find(function(err, result){
+      let books = result
+      mongoose.model('customers').find(function(err, result){
+        let customers = result
+        res.render('result.ejs', {books: books, transactions: transactions, customers: customers})
+      })
+    })
   })
 })
 
@@ -36,7 +31,7 @@ router.post('/submitbooks', function(req,res,next){
     if(err){
       res.send(error)
     } else {
-      res.redirect('/books')
+      res.redirect('/')
     }
   })
 })
@@ -46,7 +41,7 @@ router.post('/submittransactions', function(req,res,next){
     if(err){
       res.send(error)
     } else {
-      res.redirect('/transactions')
+      res.redirect('/')
     }
   })
 })
@@ -56,32 +51,82 @@ router.post('/submitcustomers', function(req,res,next){
     if(err){
       res.send(error)
     } else {
-      res.redirect('/customers')
+      res.redirect('/')
     }
   })
 })
 
-router.post('/delete/:id', function(req,res,next){
+router.post('/deletebooks/:id', function(req,res,next){
   Model.Books.remove({isbn: req.params.id}, function(err){
     if (err) {
       res.send(error)
     } else {
-      res.redirect('/books')
+      res.redirect('/')
     }
   })
 })
 
-router.post('/edit/:id', function(req,res,next){
+router.post('/editbooks/:id', function(req,res,next){
   Model.Books.findOne({isbn: req.params.id}, function(err, result){
     res.render('edit.ejs', {book: result.toJSON()})
   })
 })
 
-router.post('/updated/:id', function(req,res,next){
+router.post('/updatedbooks/:id', function(req,res,next){
   Model.Books.update({_id: req.params.id}, {isbn: req.body.isbn, title: req.body.title, author: req.body.author, category: req.body.category, stock: req.body.stock}, function(err, result){
-    res.redirect('/books')
+    res.redirect('/')
+  })
+})
+
+router.post('/deletecustomers/:id', function(req,res,next){
+  Model.Customers.remove({memberid: req.params.id}, function(err){
+    if (err) {
+      res.send(error)
+    } else {
+      res.redirect('/')
+    }
+  })
+})
+
+router.post('/editcustomers/:id', function(req,res,next){
+  Model.Customers.findOne({memberid: req.params.id}, function(err, result){
+    res.render('editcustomers.ejs', {customers: result.toJSON()})
+  })
+})
+
+router.post('/updatedcustomers/:id', function(req,res,next){
+  Model.Customers.update({_id: req.params.id}, {name: req.body.name, memberid: req.body.memberid, zipcode: req.body.zipcode, address: req.body.address, phone: req.body.phone}, function(err, result){
+    res.redirect('/')
+  })
+})
+
+router.post('/deletetransactions/:id', function(req,res,next){
+  Model.Transactions.remove({_id: req.params.id}, function(err){
+    if (err) {
+      res.send(error)
+    } else {
+      res.redirect('/')
+    }
+  })
+})
+
+router.post('/edittransactions/:id', function(req,res,next){
+
+  Model.Transactions.findOne({_id: req.params.id}, function(err, result){
+    res.render('edittransactions.ejs', {transaction: result.toJSON()})
+  })
+})
+
+router.post('/updatedtransactions/:id', function(req,res,next){
+  Model.Transactions.update({_id: req.params.id}, {name: req.body.name, memberid: req.body.memberid, zipcode: req.body.zipcode, address: req.body.address, phone: req.body.phone}, function(err, result){
+    res.redirect('/')
   })
 })
 
 
 module.exports = router
+
+
+//booklist only show 1
+//date cannot be in the correct format
+//if blank, put blank
